d905d47d045abe5a611e51792be9cf9d9d8a5a05
Hayson <heyson@sina.cn>
2017-07-04 00:41:53 +0800

    implement step 5

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -1,6 +1,7 @@
 class Admin::JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
   before_action :require_is_admin
+  layout "admin"
 
 
 
@@ -36,6 +37,18 @@ class Admin::JobsController < ApplicationController
     else
       render :edit
     end
+  end
+
+  def publish
+    @job = Job.find(params[:id])
+    @job.publish!
+    redirect_to :back
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+    @job.hide!
+    redirect_to :back
   end
 
   def destroy
